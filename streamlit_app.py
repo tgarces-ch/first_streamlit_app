e2eee4b4a8746f143d12aca2406dcc7d5f2fd596
Tomas Garces Chaparro <tgarces@soluciones.cl>
2023-02-07 17:27:23 -0300

    Se agrega biblioteca Pandas y lectura de csv fruit_macros. Se muestra en Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
+
+
+
 streamlit.title('Breakfast Favorites')
 
 
@@ -15,3 +18,12 @@ streamlit.text('🐔 Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+streamlit.dataframe(my_fruit_list)
+
