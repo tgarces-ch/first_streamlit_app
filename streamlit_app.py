dfeac56d88975f8b0c9dd2bb41a8ebf93b76587f
Tomas Garces Chaparro <tgarces@soluciones.cl>
2023-03-13 16:09:18 -0300

    Se agrega Fuityvice Api Response

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,11 @@ else:
     streamlit.dataframe(fruits_to_show)
 
 #streamlit.dataframe(my_fruit_list)
+
+
+
+# new section to display fruityvice API response
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
