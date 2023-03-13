99ebb83dc294cfe9241c9fb6192c0138661d7aba
Tomas Garces Chaparro <tgarces@soluciones.cl>
2023-03-13 17:45:32 -0300

    Se agrega mejora Fuityvice Api Response 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,13 @@ else:
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # new section to display fruityvice API response
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) # Just writes data to the screen
 
 # Take the Json version of the response and Normalize it
