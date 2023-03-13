ec2bd1dc44f83a0586500f6c214763ab02e19133
Tomas Garces Chaparro <tgarces@soluciones.cl>
2023-03-13 17:37:21 -0300

    Se agrega nuevamente Fuityvice Api Response header

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,12 @@ else:
 
 
 
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 # new section to display fruityvice API response
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
