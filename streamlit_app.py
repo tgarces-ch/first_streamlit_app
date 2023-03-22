64221dd53f73878c31664a99d2ff787e9406a59e
Tomas Garces Chaparro <tgarces@soluciones.cl>
2023-03-22 11:42:38 -0300

    Turn repeteable code lines into function

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,12 @@ import snowflake.connector
 from urllib.error import URLError
 
 
+def get_fruityvice_data (this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
 
 streamlit.title('Breakfast Favorites')
 
@@ -56,9 +62,8 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function(fruit_choice)
+        streamlit.dataframe(back_from_function)
  
 except URLError as e:
     streamlit.error()
