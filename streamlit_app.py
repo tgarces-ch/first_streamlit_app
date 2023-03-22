25d03f69df5ea6acc6d566c0ac8698fb02397b9d
Tomas Garces Chaparro <tgarces@soluciones.cl>
2023-03-22 11:21:13 -0300

    Error handling added

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,19 +51,30 @@ else:
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+ 
+except URLError as e:
+    streamlit.error()
+ 
+#streamlit.write('The user entered ', fruit_choice)
 
 # new section to display fruityvice API response
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 #streamlit.text(fruityvice_response.json()) # Just writes data to the screen
 
 # Take the Json version of the response and Normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
 
 
 # don't run anything past here while we troubleshoot
